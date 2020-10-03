8a77f42e95b61290d62327f33e4dac6269b8d089
DESKTOP-325D3AA\CAMT <pensiri_n@cmu.ac.th>
2020-10-03 15:31:11 +0700

    no message

---
+++ Assets/OptionsMenuControlScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenuControlScript : MonoBehaviour
+{
+   
+    [SerializeField] Toggle _toggleMusic;
+    [SerializeField] Toggle _toggleSFX;
+    [SerializeField] Button _backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        _toggleMusic.isOn = GameApplicationManager.Instance._isMusicEnabled;
+        _toggleSFX.isOn = GameApplicationManager.Instance.SFXEnabled;
+        
+        _toggleMusic.onValueChanged.AddListener(delegate { OnToggleMusic(_toggleMusic); });
+        _toggleSFX.onValueChanged.AddListener(delegate { OnToggleSFX(_toggleSFX); });
+        _backButton.onClick.AddListener(delegate { BackButtonClick(_backButton); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void BackButtonClick(Button button)
+    {
+        SceneManager.UnloadSceneAsync("SceneOptions");
+        GameApplicationManager.Instance.IsOptionMenuActive = false;
+    }
+    
+    public void OnToggleMusic(Toggle toggle)
+    {
+        GameApplicationManager.Instance._isMusicEnabled = _toggleMusic.isOn;
+    }
+    public void OnToggleSFX(Toggle toggle)
+    {
+        GameApplicationManager.Instance.SFXEnabled = _toggleSFX.isOn;
+    }
+}
---
+++ Assets/SingletonGameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public class SingletonGameManager : Singleton<SingletonGameManager >
+    {
+        protected SingletonGameManager(){}
+
+        public string ClassName{get;} = "SingletonGameManager";
+
+        public int GameScore{get;set;} = 0;
+
+    }
+}
+
+    
